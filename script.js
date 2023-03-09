4fa878ff1117610d88cfb858642d159591da63b0
Gylowlang <125180922+Gylowlang@users.noreply.github.com>
2023-03-09 11:55:08 +0800

    Updated

---
+++ script.js
@@ -0,0 +1,19 @@
+// Calculator functionality
+
+let result = document.getElementById('result');
+
+function insert(value) {
+	result.value += value;
+}
+
+function clearResult() {
+	result.value = '';
+}
+
+function backspace() {
+	result.value = result.value.slice(0, -1);
+}
+
+function calculate() {
+	result.value = eval(result.value);
+}
